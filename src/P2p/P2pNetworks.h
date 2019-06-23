5f4101a026122f42da117562ecf8bbe7d65c2f14
jordao <brasilpactual@gmail.com>
2019-06-23 02:43:21 -0300

    primeiro commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace Dinaro
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x39, 0x54, 0x59, 0x51, 0x54, 0x5f, 0x4e, 0x4f, 0x54, 0x85, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10 } };
+}
